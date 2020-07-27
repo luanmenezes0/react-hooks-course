d7766eff82197bef32d038aba4b0c896e2851fec
luanmenezes0 <luanmenezes99@gmail.com>
2020-07-27 15:15:33 -0300

    added add and remove list item

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import Search from "./Search";
+import IngredientList from "./IngredientList";
 
 function Ingredients() {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    fetch("https://react-hooks-update-6d4f2.firebaseio.com/ingredients.json", {
+      method: "POST",
+      body: JSON.stringify(ingredient),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevUserIngredients) => [
+          ...prevUserIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
+  };
+
+  const removeItemHandler = (id) => {
+    setUserIngredients((prevUserIngredients) =>
+      prevUserIngredients.filter((ig) => ig.id !== id)
+    );
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeItemHandler}
+        />
       </section>
     </div>
   );
